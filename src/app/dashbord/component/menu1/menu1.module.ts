c0dff672e37ae8ccf7d9606bd7eeff8a5185fd31
Nunthestar <n976apple@gmail.com>
2022-06-10 13:39:14 +0700

    upload

--- src/app/dashbord/component/menu1/menu1.module.ts
+++ src/app/dashbord/component/menu1/menu1.module.ts
@@ -2,10 +2,11 @@ import { Menu1Component } from './menu1.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Menu1RoutingModule } from './menu1-routing.module';
+import { Modal1Component } from './component/modal1/modal1.component';
 
 
 @NgModule({
-  declarations: [Menu1Component],
+  declarations: [Menu1Component, Modal1Component],
   imports: [
     CommonModule,
     Menu1RoutingModule
